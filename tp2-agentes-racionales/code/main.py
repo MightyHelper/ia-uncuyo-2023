9317c59ed6c01ba7967a234a1508c6a09e87801e
Federico Williamson - WGPC5 <federicowilliamson@hotmail.co.uk>
2023-08-28 15:19:36 -0300

    Add import guard

--- tp2-agentes-racionales/code/main.py
+++ tp2-agentes-racionales/code/main.py
@@ -1,8 +1,16 @@
-from hoover_agent import ReflexiveHooverAgent
-from hoover_env import HooverDiscreteEnvironment
-from lib.random_discrete_agent import RandomAgent
+#!/usr/bin/python3
+try:
+    from lib.random_discrete_agent import RandomAgent
+except ModuleNotFoundError as e:
+    path = "PYTHONPATH"
+    print(f"\x1b[31;1mCould not find python module '{e.name}'. Please add it to env variable {path}\x1b[0m")
+    import os
+    print(f"Current {path}: {os.getenv(path)}")
+    exit(-1)
 import pandas as pd
 import numpy as np
+from hoover_agent import ReflexiveHooverAgent
+from hoover_env import HooverDiscreteEnvironment
 
 DEBUG = False
 
