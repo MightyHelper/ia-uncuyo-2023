fb5c7ff1f2914be069af871a2883fb832b14bc55
Federico Williamson - WGPC5 <federicowilliamson@hotmail.co.uk>
2023-08-29 15:56:22 -0300

    Execute

--- tp2-agentes-racionales/code/main.py
+++ tp2-agentes-racionales/code/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+from typing import Iterable, Any
+import multiprocessing
+
 try:
     from lib.random_discrete_agent import RandomAgent
 except ModuleNotFoundError as e:
     path = "PYTHONPATH"
     print(f"\x1b[31;1mCould not find python module '{e.name}'. Please add it to env variable {path}\x1b[0m")
     import os
+
     print(f"Current {path}: {os.getenv(path)}")
     exit(-1)
 import pandas as pd
@@ -47,32 +51,70 @@ def test_agent(agent_type, env_size, env_dirt, max_time=1000):
     return env.used_time, env.get_performance()
 
 
+def dispatch_loops(loops: list[Iterable], func, current_args: list[Any]):
+    if len(loops) == 0: return func(*current_args)
+    current_loop, other_loops = loops[0], loops[1:]
+    return [dispatch_loops(other_loops, func, current_args + [i]) for i in current_loop]
+
+
+
+
+def dispatch_loops_multithread(loops: list[Iterable], func: Any):
+    cartesian_product = np.array(np.meshgrid(*loops)).T.reshape(-1, len(loops))
+    multiprocessing.set_start_method('spawn')
+    with multiprocessing.Pool() as pool:
+        # Get thread count
+        print(f"Running {len(cartesian_product)} tests, using {multiprocessing.cpu_count()} cpus = {len(cartesian_product) / multiprocessing.cpu_count()} tests per cpu")
+        # Run the function in parallel
+        return pool.starmap(func, cartesian_product)
+
+def test(agent_type: str, size: int, dirt: float, run: int, max_time: int):
+    size = int(size)
+    dirt = float(dirt)
+    run = int(run)
+    max_time = int(max_time)
+    used_time, performance = test_agent(agent_type, size, dirt, max_time)
+    return {
+        'agent': agent_type,
+        'size_value': size,
+        'size': f"{1 << size}x{1 << size}",
+        'cells': (1 << size) * (1 << size),
+        'run': run,
+        'dirt': dirt,
+        'time': used_time,
+        'performance': performance
+    }
+
+
 def benchmark_agents():
     print("Running benchmark")
-    output = pd.DataFrame(columns=['agent', 'size_value', 'size', 'cells', 'dirt', 'run', 'time', 'performance'])
     max_time = 1000
-    for agent_type in ['random', 'reflexive']:
-        for size in range(0, 8):
-            for dirt in [0.1, 0.2, 0.4, 0.8]:
-                for run in range(0, 10):
-                    used_time, performance = test_agent(agent_type, size, dirt, max_time)
-                    output = output._append({
-                        'agent': agent_type,
-                        'size_value': size,
-                        'size': f"{1 << size}x{1 << size}",
-                        'cells': (1 << size) * (1 << size),
-                        'run': run,
-                        'dirt': dirt,
-                        'time': used_time,
-                        'performance': performance
-                    }, ignore_index=True)
-                    # print(f"Agent: {agent_type} Size: {1 << size}x{1 << size}({(1 << size) * (1<<size)}), Dirt: {dirt}, Time Used: {used_time}, Performance: {performance}")
-    # print(output.to_string()) # All data
-    output = output.groupby(['agent', 'size_value', 'size', 'cells', 'dirt', 'run']).mean()
-    # print(output.to_string()) # Grouped data
-    # Average over 'run' column
-    output = output.groupby(['agent', 'size_value', 'size', 'cells', 'dirt']).mean()
-    print(output.to_string()) # Average over run data
+    iter_count = 1000
+    loops = [
+        ['random', 'reflexive'], # Agent_type
+        range(0, 8),             # Size
+        np.arange(0., 1., 0.01),        # [0.1, 0.2, 0.4, 0.8],    # Dirt %
+        range(0, iter_count),    # Iter count
+        [max_time]               # Max time
+    ]
+
+
+    output = dispatch_loops_multithread(loops, test)
+    output = pd.DataFrame(output)
+    output = output.drop(columns=['run', 'cells'])
+    # output = output.groupby(['agent', 'size_value', 'size', 'cells', 'dirt', 'run']).mean()
+    output = output.groupby(['agent', 'size', 'dirt']).mean()
+    # remove the 'run' column
+    print(output.to_string())  # Average over run data
+    output.to_csv("output.csv")
+    print("\n\n")
+    # Ungroup
+    output = output.reset_index()
+    # Sort by agent, size, dirt
+    output = output.sort_values(by=['agent', 'size_value', 'dirt'])
+    output = output.drop(columns=['size_value'])
+    # Export as md
+    print(output.to_markdown())
 
 
 if __name__ == "__main__":
